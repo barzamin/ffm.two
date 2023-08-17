1cb1b2cd8c0ce25ed4e7a145ae92c9df9dd61be9
erin petra sofiya moon <erin@hecke.rs>
2023-08-16 23:57:45 -0500

    exclude passwordHash from $page.data

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,13 +1,16 @@
 import type { Artist } from '@prisma/client';
 import { env } from '$env/dynamic/private';
-import { db } from '$lib/server/db';
+import { db, exclude } from '$lib/server/db';
 import * as jwt from 'jsonwebtoken';
 
 export function createSessionToken(artist: Artist): string {
     return jwt.sign({ artistId: artist.id }, env.JWT_SECRET, { expiresIn: '14d' });
 }
 
-export async function getUserFromSession(sessionToken: string): Promise<Artist | null> {
+// yankee with no password
+export type ArtistLite = Omit<Artist, 'passwordHash'>;
+
+export async function getUserFromSession(sessionToken: string): Promise<ArtistLite | null> {
     let token;
     try {
         token = jwt.verify(sessionToken, env.JWT_SECRET) as jwt.JwtPayload;
@@ -15,9 +18,9 @@ export async function getUserFromSession(sessionToken: string): Promise<Artist |
         return null;
     }
 
-    const artist = await db.artist.findUnique({
+    const artist = exclude(await db.artist.findUnique({
         where: { id: token.artistId },
-    });
+    }), ['passwordHash']);
 
     return artist;
 }
